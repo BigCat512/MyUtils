28e6bbc108b190af8bf14928531e9ef64b4fc138
BigCat512 <xjh@xiejiahao.com>
2023-03-31 09:55:17 +0800

    Pattern 模式匹配

---
+++ src/main/java/org/example/patter/BitOperationMatch.java
@@ -0,0 +1,46 @@
+package org.example.patter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <p>
+ * 模式匹配是通过位运算来匹配
+ *
+ * java.util.regex.Pattern#addFlag
+ * </p>
+ *
+ * @author XJH
+ * @version 1.0
+ * @since 2023/3/31
+ */
+public class BitOperationMatch {
+    /**
+     * 比如忽略大小写、多行匹配等
+     * @param args  args
+     * @return 比如忽略大小写、多行匹配等
+     * @author XJH
+     * @since 2023/3/31
+     **/
+    public static void main(String[] args) {
+        // 大小写匹配
+        // int flag = Pattern.CASE_INSENSITIVE;
+        // String regx = "Test";
+        // String input = "tEsT";
+        // Pattern compile = Pattern.compile(regx);
+        // System.out.println(compile.matcher(input).find());
+        // Pattern compile2 = Pattern.compile(regx, flag);
+        // System.out.println(compile2.matcher(input).find());
+        // 大小写忽略 + 多行匹配
+        int flag = Pattern.CASE_INSENSITIVE | Pattern.MULTILINE;
+        String regx = "^dog$";
+        String input = "cat\nDog\nTasmanian devil";
+        Pattern p1 = Pattern.compile(regx);
+        System.out.println(p1.matcher(input).find());
+        Pattern p2 = Pattern.compile(regx,Pattern.MULTILINE);
+        System.out.println(p2.matcher(input).find());
+        Pattern p3 = Pattern.compile(regx, flag);
+        System.out.println(p3.matcher(input).find());
+
+    }
+}
